0326f1931891c47641006d5608bbc0ab3a449a5a
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-01 11:59:18 +0300

    add full update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-document.getElementById('gen_pass').onclick = function gen_password(len){
-    var length_pass = document.getElementById('pass_length').value;
+function gen_password(len){
     var password = "";
-    var symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_+=";
+    var symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567891234567890-=!@#$%^&*()_+{}[]:',.<>`~йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
     for (var i = 0; i < len; i++){
         password += symbols.charAt(Math.floor(Math.random() * symbols.length));     
-    }
+    };
     return password;
-}
+};
+
+document.getElementById('gen_pass').onclick = function() {
+    var length_pass = document.getElementById('pass_length').value;
+    document.getElementById('out').innerHTML = gen_password(length_pass);
+};
